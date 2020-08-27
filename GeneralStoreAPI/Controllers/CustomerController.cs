55db3934a3890c317d9310ddaf5925e21b33a2f9
Nicholas <higherfuryx@mail.com>
2020-08-27 14:45:54 -0500

    Built out and tested Create and Read methods for Data Model

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Http;
 
 namespace GeneralStoreAPI.Controllers
@@ -12,14 +13,48 @@ namespace GeneralStoreAPI.Controllers
     {
         private StoreDbContext _context = new StoreDbContext();
 
-        // Post
+        // Post --Create
+        public IHttpActionResult Post(Customer customer)
+        {
+            // If an empty Customer is passed in
+            if (customer == null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+            // If the ModelState is not valid
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
 
-        // Get
+        // Get --Read All
+        public IHttpActionResult Get()
+        {
+            List<Customer> customers = _context.Customers.ToList();
+            if (customers.Count != 0)
+            {
+                return Ok(customers);
+            }
+            return BadRequest("Your database contains no Customers");
+        }
 
-        // Get(id)
+        // Get(id) --Read By Id
+        public IHttpActionResult Get(int id)
+        {
+            Customer customer = _context.Customers.Find(id);
+            if(customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
 
-        // Put(id)
+        // Put(id) --Update By Id
 
-        //Delete(id)
+        // Delete(id) -- Delete By Id
     }
 }
